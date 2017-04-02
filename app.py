420a403e23b01a78eecbd99fbfd9194e280e7622
Akshay Nair <phenax5@gmail.com>
2017-04-02 21:12:08 +0530

    Port set to grab from env

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from routes import initialize_routes
 
 app = Flask(__name__)
 
-ROOT_DIR= os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 initialize_routes(app)
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=8080)
+	app.run(port = os.environ.get('PORT') if os.environ.get('PORT') else 8080)
